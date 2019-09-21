8842a7ddea5dfe6e39beec4be69b3f8f9c9b29e1
Jose Ricardo <jose.ricardo@zinobe.com>
2019-09-21 16:15:42 -0500

    change

--- app/Http/Controllers/ValidateController.php
+++ app/Http/Controllers/ValidateController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Contact;
 use App\Http\Resources\ApplicationStaus;
+use App\Http\Resources\Contact as AppContact;
 use App\Http\Resources\PayMethods;
 use App\Http\Resources\PayValue;
 use App\Repositories\InformationRepository;
 use Illuminate\Database\Schema\Builder;
 use Illuminate\Http\Request;
+use MarcinOrlowski\ResponseBuilder\ResponseBuilder;
 
 class ValidateController extends Controller
 {
@@ -25,21 +27,27 @@ class ValidateController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($phone)
+    public function cellphone($phone)
     {
-        return Contact::whereCellphone($phone)->first()->applications()->first()->status()->get();
+        return ResponseBuilder::success(new AppContact(Contact::whereCellphone($phone)->first()));
     }
 
+    public function identification(Request $request)
+    {
+        $cedula = $request->cedula;
+        $phone = $request->phone;
+        return ResponseBuilder::success($this->informationRepository->validation($cedula, $phone));
+    }
 
     public function payMethods(){
-        return new PayMethods(
+        return ResponseBuilder::success(new PayMethods(
             [
                 "Efecty",
                 "Baloto",
                 "Exito",
                 "pse"
             ]
-        );
+        ));
     }
 
 
@@ -51,7 +59,7 @@ class ValidateController extends Controller
     public function applicationStatus($celphone, $last = true, $opens = true)
     {
         $this->informationRepository->celphone = $celphone;
-        return  new ApplicationStaus($this->informationRepository->applicationStatus($last, $opens));
+        return  ResponseBuilder::success(new ApplicationStaus($this->informationRepository->applicationStatus($last, $opens)));
     }
 
 
@@ -63,7 +71,7 @@ class ValidateController extends Controller
     public function payValue($celphone)
     {
         $this->informationRepository->celphone = $celphone;
-        return  new PayValue($this->informationRepository->payValue());
+        return  ResponseBuilder::success(new PayValue($this->informationRepository->payValue()));
     }
 
 
--- app/Notifications/WhatsappNotification.php
+++ app/Notifications/WhatsappNotification.php
@@ -13,14 +13,18 @@ class WhatsappNotification extends Notification
 {
     use Queueable;
 
+    private $message;
+    private $contact;
+
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(string $message, $contact)
     {
-        //
+        $this->message = $message;
+        $this->contact = $contact;
     }
 
     /**
@@ -43,12 +47,16 @@ class WhatsappNotification extends Notification
     public function toWhatsapp($notifiable)
     {
         $client = new Client();
-        $result = $client->post('your-request-uri', [
-            'form_params' => [
-                'sample-form-data' => 'value'
+        $client->post('https://go.botmaker.com/api/v1.0/message/v3', [
+            "body" => [
+                "chatPlatform" => "whatsapp",
+                "chatChannelNumber" => "573152631371",
+                "platformContactId" => $this->contact,
+                "messageText" => $this->message
             ],
             "header" => [
-
+                "access-token" => "eyJhbGciOiJIUzUxMiJ9.eyJidXNpbmVzc0lkIjoiTGluZXJ1IiwibmFtZSI6Ikp1YW4gUm96byIsImFwaSI6dHJ1ZSwiaWQiOiJVS0RSMmVvcjNJYVF3MUs3eUhrdTBwOTFyOWgxIiwiZXhwIjoxNzI2MzI2MjQ2LCJqdGkiOiJVS0RSMmVvcjNJYVF3MUs3eUhrdTBwOTFyOWgxIn0.0Rn8Gve5X2r-9Yq8vjyYVfRXjRzgc_BOnzppiZpUQOTLOakQL3G3n7rQdoC62gE8lquEFwqHFXNStGK94kPNGg",
+                "Content-Type" => "application/json"
             ]
         ]);
     }
